7ad75f8b7684dda058f15405058e500f18ccadb0
MSI-90 <s.miller_22@outlook.com>
2025-11-11 01:24:43 +0700

    Template model was edited.

--- WebResume/Entites/Models/Template.cs
+++ WebResume/Entites/Models/Template.cs
@@ -1,4 +1,5 @@
 ﻿using Entites.Enums;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 
@@ -8,6 +9,10 @@ namespace Entites.Models
   {
     [Column("template_id")]
     public Guid TemplateId { get; set; }
+
+    [Required(ErrorMessage = "Название шаблона должно быть указано.")]
+    [MinLength (3, ErrorMessage = "Минимальная длина поля сосотавляет 3 символа.")]
+    [MaxLength (50, ErrorMessage = "Максимальная длина поля наименование щаблона составляет 50 символов.")]
     public string? Name { get; set; }
     public TemplateKind TemplateKind { get; set; }
 
