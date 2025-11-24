52d4f0e06c93ea47a7bf19cd21515b641d4fe3cd
MSI-90 <s.miller_22@outlook.com>
2025-11-25 02:51:17 +0700

    feat(citizenship): update entity and configuration, add seed data migrations
    - Modified Citizenship entity and configuration
    - Added multiple migrations for seeding Citizenship data
    - Adjusted RepositoryContextModelSnapshot

--- WebResume/Entites/Models/Citizenship.cs
+++ WebResume/Entites/Models/Citizenship.cs
@@ -4,6 +4,10 @@
   {
     public Guid Id { get; set; }
     public string CountryName { get; set; } = string.Empty;
+    public string CountryNameRu {  get; set; } = string.Empty;
+    public string Alpha2Code {  get; set; } = string.Empty;
+    public string Alpha3Code {  get; set; } = string.Empty;
+    public string NumericCode {  get; set; } = string.Empty;
 
     public ICollection<ResumeCitizenship> ResumesCitizenship { get; set; } = [];
   }
